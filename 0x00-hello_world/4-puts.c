678f9a7cecc28abaff02c46c1234c0b3d3fedb83
howitcanwork <wiznono61@gmail.com>
2023-08-10 16:29:47 -0700

    hh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 
 /**
  * main - entry point
- * desciption : C program 
+ * desciption : C program
  * Return: 0
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
