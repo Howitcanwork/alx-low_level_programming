44797d1046d6be2063b4a42f125fa6aadd157df3
howitcanwork <wiznono61@gmail.com>
2023-08-11 18:09:08 -0700

    jhgb

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print all single digits base on 10 and zero
+ * Return: always return 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		putchar(x);
+	putchar('\n');
+
+	return(0);
+}
