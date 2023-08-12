47bc8a18b4b0cafde8c2d6b53a97c00f10397ebb
howitcanwork <wiznono61@gmail.com>
2023-08-11 18:58:05 -0700

    hgf

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 /**
- * main - prints all possible combinations of single-digit numbers
+ * main - prints all possible combinations of single-digit number
  * Return: always return 0
  */
 int main(void)
 {
 	int x;
 
-x = '0';
-
+	x = '0';
 	while (x <= '9')
 	{
-		putchar(x)
-			if (x != '9')
-			{
-				putchar(',');
-				putchar(' ');
-			}
+		putchar(x);
+		if (x != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 		x++;
 	}
 	putchar('\n');
 
 	return (0);
 }
+
