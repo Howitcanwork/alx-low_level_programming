b70e2c296ffc308ff80a6a011fe071bcae0ca319
howitcanwork <wiznono61@gmail.com>
2023-08-11 17:00:48 -0700

    ko

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description: C program
+ * Return: always return 0
+ */
+int main(void)
+{
+	char alphabet = 'a';
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+
+	return (0);
+
+
+char alphabet = 'a';
+while (alphabet <= 'z')
+{
+putchar(alphabet);
+alphabet++;
+}
+
+return (0);
+}}
