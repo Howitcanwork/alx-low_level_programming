e7c7a2df68de9538602e8c17c6e11215f57725b4
howitcanwork <wiznono61@gmail.com>
2023-08-15 19:11:39 -0700

    jshsb

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,20 +10,21 @@ void times_table(void)
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar(48);
-		for (multi = 1; multi <= 9; multi++)
+for (multi = 1; multi <= 9; multi++)
 		{
 			_putchar(',');
 			_putchar(' ');
 
 			prod = num * multi;
+			
 			if (prod <= 9)
 			{
 				_putchar(' ');
 			}
 			else
 			{
-				_putchar((prod / 10) + 48);
-				_putchar((prod % 10) + 48);
+			_putchar((prod / 10) + 48);
+			_putchar((prod % 10) + 48);
 			}
 		}
 	_putchar('\n');
