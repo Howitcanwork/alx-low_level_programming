8054fdf96c8a327ad4b8cd8927299d69fcf811c2
howitcanwork <wiznono61@gmail.com>
2023-08-15 19:46:38 -0700

    gg

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -27,6 +27,7 @@ void times_table(void)
 				_putchar(c + '0');
 			}
 			else
+			{
 				_putchar(c + '0');
 		}
 		if (b != 9)
@@ -35,7 +36,8 @@ void times_table(void)
 			_putchar(' ');
 		}
 		b++;
-	}
+		}
 	_putchar('\n');
 	a++;
+	}
 }
