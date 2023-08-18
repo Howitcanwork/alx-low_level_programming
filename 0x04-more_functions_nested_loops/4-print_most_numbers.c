c9c0517095eed8520dff49b0c163c4894346497f
howitcanwork <wiznono61@gmail.com>
2023-08-17 17:05:22 -0700

    print most num

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers - function
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i == 50 || i == 52)
+			i++;
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+
