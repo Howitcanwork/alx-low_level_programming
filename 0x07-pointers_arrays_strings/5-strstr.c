b8a06faba176b07c08549336fa383979645c799a
howitcanwork <wiznono61@gmail.com>
2023-08-28 15:04:10 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * *_strstr - function
+ * @needle: substring
+ * @haystack: string
+ * Return: pointer
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*h == *n && *n != '\0')
+		{
+			h++;
+			n++;
+		}
+		if (*n != '\0')
+			return (haystack);
+	}
+	return (NULL);
+}
