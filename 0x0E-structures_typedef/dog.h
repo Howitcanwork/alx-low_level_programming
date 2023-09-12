f0def5208516fa08a17c9ab1314a557299092b2d
howitcanwork <wiznono61@gmail.com>
2023-09-11 17:02:43 -0700

    new

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Description: dog struct in long kitty world
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
