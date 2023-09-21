9fb7dbcf6ae1b5132757b7cb4dd90c927f1b5efa
howitcanwork <wiznono61@gmail.com>
2023-09-21 16:01:35 -0700

    ms

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,7 +10,7 @@ int _strlen(char *s)
 	int i = 0;
 
 	if (!s)
-		returb (0);
+		return (0);
 	while (*s++)
 		i++;
 	return (i);
@@ -25,11 +25,11 @@ size_t print_list(const list_t *h)
 {
 	size_t i = 0;
 
-	while (p)
+	while (h)
 	{
-		printf("[%d] %s\n", _strlen(p->str), p->str ? p->str : "(nil)");
-		p = p->next;
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+		h = h->next;
 		i++;
 	}
 	return (i);
-	ls}
+}
