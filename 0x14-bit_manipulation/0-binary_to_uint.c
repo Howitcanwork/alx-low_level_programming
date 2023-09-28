d56cb8b44252971d319ffcfd5dee30a9b9cec3bf
howitcanwork <wiznono61@gmail.com>
2023-09-28 16:51:18 -0700

    updated

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,16 +7,16 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int lu;
-	unsigned int va = 0;
+	int i;
+	unsigned int dec_val = 0;
 
 	if (!b)
 		return (0);
-	for (i = 0; b[lu]; lu++)
+	for (i = 0; b[i]; i++)
 	{
-		if (b[lu] < '0' || b[lu] > '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		va = 2 * va + (b[lu] - '0');
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
-	return (va);
+	return (dec_val);
 }
