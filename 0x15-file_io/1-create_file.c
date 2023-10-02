5798dce248e712f5810e1325557e6d1f41823540
howitcanwork <wiznono61@gmail.com>
2023-10-02 10:02:25 -0700

    new

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,8 +23,8 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = "";
 
-	for (nletters = 0; text_content[nletters]; nletters++);
-	twr = write(grd, text_content, nletters);
+	for (nletters = 0; text_content[nletters]; nletters++)
+		twr = write(grd, text_content, nletters);
 
 	if (twr == -1)
 		return (-1);
