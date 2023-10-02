78405dbed1c94981316478c9042976d84dd0bb93
howitcanwork <wiznono61@gmail.com>
2023-10-02 10:14:06 -0700

    new

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -4,8 +4,7 @@
  * append_text_to_file - appends text at the end of a file
  * @filename: filename.
  * @text_content: added content.
- * Return: 1 if the file exists. -1 if the fails does not exist
- * or if it fails.
+ * Return: 1 if the file exists. -1 if the fails does not exist or if it fails.
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
@@ -26,7 +25,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		for (nletters = 0; text_content[nletters]; nletters++)
 			;
 
-		twr = write(fd, text_content, nletters);
+		twr = write(grd, text_content, nletters);
 
 		if (twr == -1)
 			return (-1);
